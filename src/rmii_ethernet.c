abbafd51c738f7e0cdb4925167dac490950ca2f5
Maxime Borges <contact@maximeborg.es>
2021-12-29 22:21:47 +0100

    WIP

--- src/rmii_ethernet.c
+++ src/rmii_ethernet.c
@@ -6,9 +6,12 @@
 
 #include <string.h>
 
+#include "lan8720a.h"
+
 #include "hardware/dma.h"
 
 #include "pico/stdlib.h"
+#include "pico/time.h"
 #include "pico/unique_id.h"
 
 #include "lwip/etharp.h"
@@ -45,7 +48,6 @@ static int phy_address = 0;
 
 static uint8_t rx_frame[1518];
 static uint8_t tx_frame[1518];
-static uint8_t tx_frame_bits[1538 * 4];
 
 static const uint32_t ethernet_polynomial_le = 0xedb88320U;
 
@@ -219,10 +221,16 @@ void netif_rmii_ethernet_mdio_write(int addr, int reg, int val)
 
 static err_t netif_rmii_ethernet_output(struct netif *netif, struct pbuf *p)
 {
-    uint tot_len = 0;
+    // Wait until the buffer is again available
+    // TODO: use a ping-pong buffer ?
+    dma_channel_wait_for_finish_blocking(tx_dma_chan);
 
+    // Clean up the buffer
     memset(tx_frame, 0x00, sizeof(tx_frame));
 
+    // Retrieve the data to send and copy in TX buffer
+    // TODO: check for overflow?
+    uint tot_len = 0;
     for (struct pbuf *q = p; q != NULL; q = q->next) {
         memcpy(tx_frame + tot_len, q->payload, q->len);
 
@@ -233,62 +241,39 @@ static err_t netif_rmii_ethernet_output(struct netif *netif, struct pbuf *p)
         }
     }
 
+    // Minimal Ethernet payload is 64 bytes, 4-bytes CRC included
+    // Pad the payload to 64-4=60 bytes, and then add the CRC
     if (tot_len < 60) {
-        // pad
         tot_len = 60;
     }
 
+    // Append the CRC to the frame
     uint crc = ethernet_frame_crc(tx_frame, tot_len);
-
-    // printf("TX: ");
-    // for (int i = 0; i < tot_len; i++) {
-    //     printf("%02X", tx_frame[i]);
-    // }
-    // printf("\n");
-
-    dma_channel_wait_for_finish_blocking(tx_dma_chan);
-
-    int index = 0;
-
-    for (int i = 0; i < 31; i++) {
-        tx_frame_bits[index++] = 0x05;
-    }
-
-    for (int i = 0; i < 1; i++) {
-        tx_frame_bits[index++] = 0x07;
-    }
-
-    for (int i = 0; i < tot_len; i++) {
-        uint8_t b = tx_frame[i];
-
-        tx_frame_bits[index++] = 0x04 | ((b >> 0) & 0x03);
-        tx_frame_bits[index++] = 0x04 | ((b >> 2) & 0x03);
-        tx_frame_bits[index++] = 0x04 | ((b >> 4) & 0x03);
-        tx_frame_bits[index++] = 0x04 | ((b >> 6) & 0x03);
-    }
-
     for (int i = 0; i < 4; i++) {
-        uint8_t b = ((uint8_t*)&crc)[i];
-
-        tx_frame_bits[index++] = 0x04 | ((b >> 0) & 0x03);
-        tx_frame_bits[index++] = 0x04 | ((b >> 2) & 0x03);
-        tx_frame_bits[index++] = 0x04 | ((b >> 4) & 0x03);
-        tx_frame_bits[index++] = 0x04 | ((b >> 6) & 0x03);
+        tx_frame[tot_len++] = ((uint8_t*)&crc)[i];
     }
+
+    printf("TX: ");
+    for (int i = 0; i < tot_len; i++) {
+        printf("%02b%02b%02b%02b", 
+            (tx_frame[i] >> 0) & 0b11,
+            (tx_frame[i] >> 2) & 0b11,
+            (tx_frame[i] >> 4) & 0b11,
+            (tx_frame[i] >> 6) & 0b11
+        );
+    }
+    printf("\n");
+
     
-    for (int i = 0; i < (12 * 4); i++) {
-        tx_frame_bits[index++] = 0x00;
-    }
 
+    // Setup and start the DMA to send the frame via the PIO RMII tansmitter
     dma_channel_configure(
         tx_dma_chan, &tx_dma_channel_config,
         ((uint8_t*)&PICO_RMII_ETHERNET_PIO->txf[PICO_RMII_ETHERNET_SM_TX]) + 3,
-        tx_frame_bits,
-        index,
-        false
+        tx_frame,
+        tot_len,
+        true
     );
-
-    dma_channel_start(tx_dma_chan);
 
     return ERR_OK;
 }
@@ -302,6 +287,7 @@ static void netif_rmii_ethernet_rx_dv_falling_callback(uint gpio, uint32_t event
 }
 
 static err_t netif_rmii_ethernet_low_init(struct netif *netif) {
+    // Prepare the interface
     rmii_eth_netif = netif;
 
     netif->linkoutput = netif_rmii_ethernet_output;
@@ -309,6 +295,7 @@ static err_t netif_rmii_ethernet_low_init(struct netif *netif) {
     netif->mtu        = 1500; 
     netif->flags      = NETIF_FLAG_BROADCAST | NETIF_FLAG_ETHARP | NETIF_FLAG_ETHERNET | NETIF_FLAG_IGMP | NETIF_FLAG_MLD6;
 
+    // Setup the MAC address
     if (PICO_RMII_ETHERNET_MAC_ADDR != NULL) {
         memcpy(netif->hwaddr, PICO_RMII_ETHERNET_MAC_ADDR, 6);
     } else {
@@ -322,13 +309,13 @@ static err_t netif_rmii_ethernet_low_init(struct netif *netif) {
         netif->hwaddr[2] = 0xeb;
         memcpy(&netif->hwaddr[3], &board_id.id[5], 3);
     }
-    
-    
     netif->hwaddr_len = ETH_HWADDR_LEN;
-    
+
+    // Init the RMII PIO programs
     rx_sm_offset = pio_add_program(PICO_RMII_ETHERNET_PIO, &rmii_ethernet_phy_rx_data_program);
     tx_sm_offset = pio_add_program(PICO_RMII_ETHERNET_PIO, &rmii_ethernet_phy_tx_data_program);
 
+    // Configure the DMA channels
     rx_dma_chan = dma_claim_unused_channel(true);
     tx_dma_chan = dma_claim_unused_channel(true);
 
@@ -346,20 +333,38 @@ static err_t netif_rmii_ethernet_low_init(struct netif *netif) {
     channel_config_set_dreq(&tx_dma_channel_config, pio_get_dreq(PICO_RMII_ETHERNET_PIO, PICO_RMII_ETHERNET_SM_TX, true));
     channel_config_set_transfer_data_size(&tx_dma_channel_config, DMA_SIZE_8);
 
-    rmii_ethernet_phy_tx_init(PICO_RMII_ETHERNET_PIO, PICO_RMII_ETHERNET_SM_TX, tx_sm_offset, PICO_RMII_ETHERNET_TX_PIN);
+    // Configure the RMII TX state machine
+    rmii_ethernet_phy_tx_init(PICO_RMII_ETHERNET_PIO, PICO_RMII_ETHERNET_SM_TX, tx_sm_offset, PICO_RMII_ETHERNET_TX_PIN, 10);
 
+    // Retrieve the LAN8720A address
     for (int i = 0; i < 32; i++) {
         if (netif_rmii_ethernet_mdio_read(i, 0) != 0xffff) {
             phy_address = i;
-
             break;
         }
     }
 
     // netif_rmii_ethernet_mdio_write(phy_address, 0, 0x2000); // 10 Mbps, auto negeotiate disabled
 
-    netif_rmii_ethernet_mdio_write(phy_address, 4, 0x61);
-    netif_rmii_ethernet_mdio_write(phy_address, 0, 0x1000);
+    // Default mode is 10Mbps, auto-negociate disabled
+    // Uncomment this to switch to 100Mbps, auto-negociate disabled
+    // netif_rmii_ethernet_mdio_write(phy_address, LAN8720A_BASIC_CONTROL_REG, 0x2000); // 100 Mbps, auto-negeotiate disabled
+
+    // Or keep the following config to auto-negotiate 10/100Mbps
+    // 0b0000_0001_1110_0001
+    //           | |||⁻⁻⁻⁻⁻\__ 0b00001=IEEE 802.3
+    //           | || \_______ 10BASE-T ability
+    //           | | \________ 10BASE-T Full-Duplex ability
+    //           |  \_________ 100BASE-T ability
+    //            \___________ 100BASE-T Full-Duplex ability
+    netif_rmii_ethernet_mdio_write(phy_address, LAN8720A_AUTO_NEGO_REG, 
+        LAN8720A_AUTO_NEGO_REG_IEEE802_3
+        // TODO: the PIO RX and TX are hardcoded to 100Mbps, make it configurable to uncomment this
+        // | LAN8720A_AUTO_NEGO_REG_10_ABI | LAN8720A_AUTO_NEGO_REG_10_FD_ABI
+        | LAN8720A_AUTO_NEGO_REG_100_ABI | LAN8720A_AUTO_NEGO_REG_100_FD_ABI
+    );
+    // Enable auto-negotiate
+    netif_rmii_ethernet_mdio_write(phy_address, LAN8720A_BASIC_CONTROL_REG, 0x1000);
 
     return ERR_OK;
 }
@@ -394,6 +399,17 @@ void netif_rmii_ethernet_poll() {
         uint rx_frame_length = ethernet_frame_length(rx_frame, sizeof(rx_frame));
 
         if (rx_frame_length) {
+            printf("RX: ");
+            for (int i = 0; i < rx_frame_length; i++) {
+                printf("%02b%02b%02b%02b", 
+                    (rx_frame[i] >> 0) & 0b11,
+                    (rx_frame[i] >> 2) & 0b11,
+                    (rx_frame[i] >> 4) & 0b11,
+                    (rx_frame[i] >> 6) & 0b11
+                );
+            }
+            printf("\n");
+            
             // printf("RX: ");
             // for (int i = 0; i < rx_frame_length + 4; i++) {
             //     printf("%02X", rx_frame[i]);
@@ -421,7 +437,7 @@ void netif_rmii_ethernet_poll() {
 
         dma_channel_start(rx_dma_chan);
 
-        rmii_ethernet_phy_rx_init(PICO_RMII_ETHERNET_PIO, PICO_RMII_ETHERNET_SM_RX, rx_sm_offset, PICO_RMII_ETHERNET_RX_PIN);
+        rmii_ethernet_phy_rx_init(PICO_RMII_ETHERNET_PIO, PICO_RMII_ETHERNET_SM_RX, rx_sm_offset, PICO_RMII_ETHERNET_RX_PIN, 10);
 
         gpio_set_irq_enabled_with_callback(PICO_RMII_ETHERNET_RX_PIN + 2, GPIO_IRQ_EDGE_FALL, true, &netif_rmii_ethernet_rx_dv_falling_callback);
     }
